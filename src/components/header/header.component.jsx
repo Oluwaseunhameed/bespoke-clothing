2afbc6dd86c603106fb5f418b84ae28f970b955c
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-25 08:39:19 +0100

    added redux, reselect for selectors, implemented all cart, addToCart functionality and components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/bespoke-clothing.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -15,10 +21,10 @@ const Header = ({ currentUser }) => (
         <div className='options'>
             <Link className='option' to='/shop'>
                 SHOP
-        </Link>
+      </Link>
             <Link className='option' to='/shop'>
                 CONTACT
-        </Link>
+      </Link>
             {currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}>
                     SIGN OUT
@@ -28,8 +34,15 @@ const Header = ({ currentUser }) => (
                         SIGN IN
                     </Link>
                 )}
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 );
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import Directory from '../../components/directory-menu/directory-menu.component'
+import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 
 const HomePage = ({ history }) => {
     return (
         <div className="homepage">
-            <Directory history={history} />
+            <Directory />
         </div>
     )
 }
